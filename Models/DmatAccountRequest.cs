0796f6a171cf55af449b37679efa595097af7ed5
SreeramSreenubabu <sreeramsreenubabu77@gmail.com>
2024-12-09 21:48:54 +0530

    Update DmatAccountRequest.cs

--- Models/DmatAccountRequest.cs
+++ Models/DmatAccountRequest.cs
@@ -9,7 +9,6 @@ namespace DmatAccountApi.Models
         public string AadharNumber { get; set; }      // Mandatory
         public string DateOfBirth { get; set; }       // Mandatory (ISO 8601 format)
         public string Address { get; set; }           // Mandatory
-
         public string Occupation { get; set; }        // Optional
         public decimal? AnnualIncome { get; set; }    // Optional
         public string NomineeName { get; set; }       // Optional
